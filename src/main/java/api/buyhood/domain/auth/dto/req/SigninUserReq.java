c9d174a0f5f36190e89bcce7e8fb0563829a252b
KimKiHong-1111 <rlghd0157@gmail.com>
2025-05-27 10:28:24 +0900

    feat: Implement user signin (1)

---
+++ src/main/java/api/buyhood/domain/auth/dto/req/SigninUserReq.java
@@ -0,0 +1,5 @@
+package api.buyhood.domain.auth.dto.req;
+
+public class SigninUserReq {
+
+}
---
+++ src/main/java/api/buyhood/domain/seller/service/SellerService.java
@@ -0,0 +1,8 @@
+package api.buyhood.domain.seller.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SellerService {
+
+}
