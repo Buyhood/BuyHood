0b459c292d3a6cb7fbb34e9ac69f2306328b7e2e
dereck-jun <yjpark0805@naver.com>
2025-05-27 11:01:21 +0900

    fix: Remove traceId applied to the wrong location

--- src/main/java/api/buyhood/global/common/handler/GlobalExceptionHandler.java
+++ src/main/java/api/buyhood/global/common/handler/GlobalExceptionHandler.java
@@ -5,7 +5,6 @@ import api.buyhood.global.common.dto.Response;
 import api.buyhood.global.common.exception.InvalidRequestException;
 import api.buyhood.global.common.exception.enums.ErrorCode;
 import jakarta.servlet.http.HttpServletResponse;
-import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -19,10 +18,7 @@ public class GlobalExceptionHandler {
 		InvalidRequestException ire,
 		HttpServletResponse response
 	) {
-		log.error("[InvalidRequestException] traceId: {}, cause: ",
-			UUID.randomUUID().toString().substring(0, 7),
-			ire.getCause()
-		);
+		log.error("[InvalidRequestException] cause: ", ire);
 		ErrorCode errorCode = ire.getErrorCode();
 		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
 		return Response.error(new CustomExceptionDto(errorCode.getCode(), errorCode.getMessage()));
