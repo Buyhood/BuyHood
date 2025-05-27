3a1836f1302a3b28901772ad3947313083ede81f
dereck-jun <yjpark0805@naver.com>
2025-05-27 11:29:49 +0900

    fix: Changing exception handling handlers and modifying the ErrorCode field

--- src/main/java/api/buyhood/global/common/handler/GlobalExceptionHandler.java
+++ src/main/java/api/buyhood/global/common/handler/GlobalExceptionHandler.java
@@ -2,7 +2,7 @@ package api.buyhood.global.common.handler;
 
 import api.buyhood.global.common.dto.CustomExceptionDto;
 import api.buyhood.global.common.dto.Response;
-import api.buyhood.global.common.exception.InvalidRequestException;
+import api.buyhood.global.common.exception.BaseException;
 import api.buyhood.global.common.exception.enums.ErrorCode;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -13,14 +13,14 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
-	@ExceptionHandler(value = InvalidRequestException.class)
+	@ExceptionHandler(value = BaseException.class)
 	public Response<CustomExceptionDto> invalidRequestException(
-		InvalidRequestException ire,
+		BaseException be,
 		HttpServletResponse response
 	) {
-		log.error("[InvalidRequestException] cause: ", ire);
-		ErrorCode errorCode = ire.getErrorCode();
-		response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+		log.error("[{}] cause: ", be.getClass().getSimpleName(), be);
+		ErrorCode errorCode = be.getErrorCode();
+		response.setStatus(errorCode.getStatus().value());
 		return Response.error(new CustomExceptionDto(errorCode.getCode(), errorCode.getMessage()));
 	}
 }
