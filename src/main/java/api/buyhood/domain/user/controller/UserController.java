b15d42e200befa222de6f3d827ade2a8c1e8fc90
KimKiHong-1111 <rlghd0157@gmail.com>
2025-05-28 12:22:44 +0900

    feat(MFLP-21): Implementing getUser & getAllUsers

--- src/main/java/api/buyhood/domain/user/controller/UserController.java
+++ src/main/java/api/buyhood/domain/user/controller/UserController.java
@@ -1,8 +1,15 @@
 package api.buyhood.domain.user.controller;
 
+import api.buyhood.domain.user.dto.res.GetUserRes;
 import api.buyhood.domain.user.service.UserService;
+import api.buyhood.global.common.dto.Response;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,4 +19,19 @@ public class UserController {
 
 	private final UserService userService;
 
+	@GetMapping("/v1/users/{userId}")
+	public Response<GetUserRes> getUser(@PathVariable @Valid Long userId) {
+		GetUserRes getUserRes = userService.getUser(userId);
+
+		return Response.ok(getUserRes);
+	}
+
+	@GetMapping("v1/users")
+	public Response<Page<GetUserRes>> getUsers(
+		@RequestParam(defaultValue = "0") int page,
+		@RequestParam(defaultValue = "10") int size) {
+		Page<GetUserRes> getUserRes = userService.getAllUsers(page, size);
+		
+		return Response.ok(getUserRes);
+	}
 }
--- src/main/java/api/buyhood/domain/user/repository/UserRepository.java
+++ src/main/java/api/buyhood/domain/user/repository/UserRepository.java
@@ -1,9 +1,15 @@
 package api.buyhood.domain.user.repository;
 
 import api.buyhood.domain.user.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	boolean existsByEmail(String email);
+
+	@Query("SELECT u FROM User u WHERE u.deletedAt IS NULL ")
+	Page<User> findAllActiveUsers(Pageable pageable);
 }
