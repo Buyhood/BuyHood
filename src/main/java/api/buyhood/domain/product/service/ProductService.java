a721d56900f5541edcf2494a5ee02f11906d61a5
dereck-jun <yjpark0805@naver.com>
2025-05-28 23:03:33 +0900

    feat(MFLP-29): Implementing the Product Get API

--- src/main/java/api/buyhood/domain/product/service/ProductService.java
+++ src/main/java/api/buyhood/domain/product/service/ProductService.java
@@ -1,5 +1,7 @@
 package api.buyhood.domain.product.service;
 
+import api.buyhood.domain.product.dto.response.GetProductRes;
+import api.buyhood.domain.product.dto.response.PageProductRes;
 import api.buyhood.domain.product.dto.response.RegisteringProductRes;
 import api.buyhood.domain.product.entity.Category;
 import api.buyhood.domain.product.entity.Product;
@@ -7,7 +9,12 @@ import api.buyhood.domain.product.repository.CategoryRepository;
 import api.buyhood.domain.product.repository.ProductRepository;
 import api.buyhood.global.common.exception.NotFoundException;
 import api.buyhood.global.common.exception.enums.CategoryErrorCode;
+import api.buyhood.global.common.exception.enums.ProductErrorCode;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -45,4 +52,26 @@ public class ProductService {
 		return RegisteringProductRes.of(product, category != null ? category.getName() : null);
 	}
 
+	@Transactional(readOnly = true)
+	public GetProductRes getProduct(Long productId) {
+		Product product = productRepository.findById(productId)
+			.orElseThrow(() -> new NotFoundException(ProductErrorCode.PRODUCT_NOT_FOUND));
+		return GetProductRes.of(product, product.getCategory());
+	}
+
+	@Transactional(readOnly = true)
+	public Page<PageProductRes> getAllProducts(Pageable pageable) {
+		PageRequest pageRequest =
+			PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Direction.ASC, "name");
+		Page<Product> productPage = productRepository.findAll(pageRequest);
+		return PageProductRes.of(productPage);
+	}
+
+	@Transactional(readOnly = true)
+	public Page<PageProductRes> getProductByKeyword(String keyword, Pageable pageable) {
+		PageRequest pageRequest =
+			PageRequest.of(pageable.getPageNumber(), pageable.getPageSize(), Direction.ASC, "name");
+		Page<Product> productPage = productRepository.findByKeyword(keyword, pageRequest);
+		return PageProductRes.of(productPage);
+	}
 }
