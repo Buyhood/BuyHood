b6a847e0f50f2889f403fae0a04dc84f1a2c04fe
dereck-jun <yjpark0805@naver.com>
2025-05-30 01:07:11 +0900

    feat(MFLP-40): Implement the store get API

--- src/main/java/api/buyhood/domain/store/controller/StoreController.java
+++ src/main/java/api/buyhood/domain/store/controller/StoreController.java
@@ -1,14 +1,22 @@
 package api.buyhood.domain.store.controller;
 
 import api.buyhood.domain.store.dto.request.RegisteringStoreReq;
+import api.buyhood.domain.store.dto.response.GetStoreRes;
+import api.buyhood.domain.store.dto.response.PageStoreRes;
 import api.buyhood.domain.store.dto.response.RegisteringStoreRes;
 import api.buyhood.domain.store.service.StoreService;
 import api.buyhood.global.common.dto.Response;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -30,4 +38,26 @@ public class StoreController {
 		);
 		return Response.ok(response);
 	}
+
+	@GetMapping("/v1/stores/{storeId}")
+	public Response<GetStoreRes> getStore(@PathVariable Long storeId) {
+		GetStoreRes response = storeService.getStore(storeId);
+		return Response.ok(response);
+	}
+
+	@GetMapping("/v1/stores")
+	public Response<Page<PageStoreRes>> getAllStore(@PageableDefault Pageable pageable) {
+		Page<PageStoreRes> response = storeService.getAllStore(pageable);
+		return Response.ok(response);
+	}
+
+	@GetMapping("/v1/stores/keyword")
+	public Response<Page<PageStoreRes>> getStoreByKeyword(
+		@RequestParam(required = false) String keyword,
+		@PageableDefault Pageable pageable
+	) {
+		Page<PageStoreRes> response = storeService.getStoreByKeyword(keyword, pageable);
+		return Response.ok(response);
+	}
+
 }
