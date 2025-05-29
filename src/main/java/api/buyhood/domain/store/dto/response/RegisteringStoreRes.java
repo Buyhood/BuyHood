08130fa08b5b574ecec449fd8dfbdc81479070f0
dereck-jun <yjpark0805@naver.com>
2025-05-29 23:14:25 +0900

    feat(MFLP-39): Implement the Store Registration API

---
+++ src/main/java/api/buyhood/domain/store/dto/response/RegisteringStoreRes.java
@@ -0,0 +1,32 @@
+package api.buyhood.domain.store.dto.response;
+
+import api.buyhood.domain.store.entity.Store;
+import java.time.LocalTime;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class RegisteringStoreRes {
+
+	private final Long storeId;
+	private final String storeName;
+	private final String address;
+	private final Long sellerId;
+	private final String description;
+	private final LocalTime openedAt;
+	private final LocalTime closedAt;
+
+	public static RegisteringStoreRes of(Store store, Long sellerId) {
+		return new RegisteringStoreRes(
+			store.getId(),
+			store.getName(),
+			store.getAddress(),
+			sellerId,
+			store.getDescription(),
+			store.getOpenedAt(),
+			store.getClosedAt()
+		);
+	}
+}
