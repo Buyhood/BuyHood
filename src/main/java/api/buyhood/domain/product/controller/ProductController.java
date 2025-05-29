0c596186fa18626c30bb7c7cf6f10fe63a1e43ef
dereck-jun <yjpark0805@naver.com>
2025-05-29 12:14:31 +0900

    feat(MFLP-31): Implement the product modification API

--- src/main/java/api/buyhood/domain/product/controller/ProductController.java
+++ src/main/java/api/buyhood/domain/product/controller/ProductController.java
@@ -1,5 +1,6 @@
 package api.buyhood.domain.product.controller;
 
+import api.buyhood.domain.product.dto.request.PatchProductReq;
 import api.buyhood.domain.product.dto.request.RegisteringProductReq;
 import api.buyhood.domain.product.dto.response.GetProductRes;
 import api.buyhood.domain.product.dto.response.PageProductRes;
@@ -12,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -57,5 +59,17 @@ public class ProductController {
 	) {
 		Page<PageProductRes> response = productService.getProductByKeyword(keyword, pageable);
 		return Response.ok(response);
+	}
+
+	@PatchMapping("/v1/products/{productId}")
+	public void patchProduct(@PathVariable Long productId, @RequestBody PatchProductReq request) {
+		productService.patchProduct(
+			productId,
+			request.getProductName(),
+			request.getPrice(),
+			request.getCategoryId(),
+			request.getDescription(),
+			request.getStock()
+		);
 	}
 }
