92bd9d0173573eb400e9d733e88f252d37744f44
dereck-jun <116302666+dereck-jun@users.noreply.github.com>
2025-05-31 23:43:15 +0900

    refactor(MFLP-48): Modify category name duplicate check logic

--- src/main/java/api/buyhood/domain/product/service/CategoryService.java
+++ src/main/java/api/buyhood/domain/product/service/CategoryService.java
@@ -6,6 +6,7 @@ import api.buyhood.domain.product.dto.response.PageCategoryRes;
 import api.buyhood.domain.product.entity.Category;
 import api.buyhood.domain.product.repository.CategoryProductRepository;
 import api.buyhood.domain.product.repository.CategoryRepository;
+import api.buyhood.global.common.exception.ConflictException;
 import api.buyhood.global.common.exception.InvalidRequestException;
 import api.buyhood.global.common.exception.NotFoundException;
 import api.buyhood.global.common.exception.enums.CategoryErrorCode;
@@ -24,16 +25,28 @@ public class CategoryService {
 	private final CategoryRepository categoryRepository;
 	private final CategoryProductRepository categoryProductRepository;
 
+	/**
+	 * 카테고리 생성
+	 *
+	 * @param categoryName 카테고리 이름 (필수)
+	 * @param parentId     부모 카테고리 ID 값 (선택)
+	 * @author dereck-jun
+	 */
 	@Transactional
 	public CreateCategoryRes createCategory(String categoryName, Long parentId) {
 		Category parent = null;
 		if (parentId != null && parentId != 0) {
 			parent = categoryRepository.findById(parentId)
 				.orElseThrow(() -> new NotFoundException(CategoryErrorCode.CATEGORY_NOT_FOUND));
-		}
-
-		if (categoryRepository.existsByParentIdAndName(categoryName, parentId)) {
-			throw new InvalidRequestException(CategoryErrorCode.DUPLICATE_CATEGORIES);
+			// 하위 카테고리 중복 체크
+			if (categoryRepository.existsByParentIdAndName(parentId, categoryName)) {
+				throw new InvalidRequestException(CategoryErrorCode.DUPLICATE_CATEGORIES);
+			}
+		} else {
+			// 최상위 카테고리 중복 체크
+			if (categoryRepository.existsByParentIsNullAndName(categoryName)) {
+				throw new InvalidRequestException(CategoryErrorCode.DUPLICATE_CATEGORIES);
+			}
 		}
 
 		int depth = parent == null ? 0 : parent.getDepth() + 1;
@@ -51,6 +64,12 @@ public class CategoryService {
 		return CreateCategoryRes.of(categoryRepository.save(category));
 	}
 
+	/**
+	 * 카테고리 단건 조회
+	 *
+	 * @param categoryId 조회할 카테고리 ID
+	 * @author dereck-jun
+	 */
 	@Transactional(readOnly = true)
 	public GetCategoryRes getCategory(Long categoryId) {
 		Category getCategory = categoryRepository.findById(categoryId)
@@ -58,6 +77,12 @@ public class CategoryService {
 		return GetCategoryRes.of(getCategory);
 	}
 
+	/**
+	 * 카테고리 전체 조회
+	 *
+	 * @param pageable
+	 * @author dereck-jun
+	 */
 	@Transactional(readOnly = true)
 	public Page<PageCategoryRes> getAllCategories(Pageable pageable) {
 		PageRequest pageRequest =
@@ -65,6 +90,13 @@ public class CategoryService {
 		return PageCategoryRes.of(categoryRepository.findAll(pageRequest));
 	}
 
+	/**
+	 * 카테고리 depth 별 조회
+	 *
+	 * @param depth    조회할 카테고리의 depth
+	 * @param pageable
+	 * @author dereck-jun
+	 */
 	@Transactional(readOnly = true)
 	public Page<PageCategoryRes> getDepthCategories(int depth, Pageable pageable) {
 		PageRequest pageRequest =
@@ -72,13 +104,40 @@ public class CategoryService {
 		return PageCategoryRes.of(categoryRepository.getCategoriesByDepth(depth, pageRequest));
 	}
 
+	/**
+	 * 카테고리 수정
+	 *
+	 * @param categoryId      수정할 카테고리 ID
+	 * @param newCategoryName 수정할 카테고리 이름
+	 * @author dereck-jun
+	 */
 	@Transactional
 	public void patchCategory(Long categoryId, String newCategoryName) {
 		Category getCategory = categoryRepository.findById(categoryId)
 			.orElseThrow(() -> new NotFoundException(CategoryErrorCode.CATEGORY_NOT_FOUND));
+
+		// 부모가 있으면, 해당 부모 아래에서 중복 체크
+		if (getCategory.getParent() != null) {
+			Long parentId = getCategory.getParent().getId();
+			if (categoryRepository.existsByParentIdAndName(parentId, newCategoryName)) {
+				throw new ConflictException(CategoryErrorCode.DUPLICATE_CATEGORIES);
+			}
+		} else {
+			// 부모가 없으면, 최상위 카테고리끼리 중복 체크
+			if (categoryRepository.existsByParentIsNullAndName(newCategoryName)) {
+				throw new ConflictException(CategoryErrorCode.DUPLICATE_CATEGORIES);
+			}
+		}
+
 		getCategory.patchCategory(newCategoryName);
 	}
 
+	/**
+	 * 카테고리 삭제 (물리적 삭제)
+	 *
+	 * @param categoryId 삭제할 카테고리 ID
+	 * @author dereck-jun
+	 */
 	@Transactional
 	public void deleteCategory(Long categoryId) {
 		Category getCategory = categoryRepository.findById(categoryId)
