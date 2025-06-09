2523afd7f855d371ecde19763da5abd5f9d794d7
KimKiHong-1111 <rlghd0157@gmail.com>
2025-06-09 15:45:17 +0900

    feat(MFLP-82): create module base

---
+++ buyhood-api-gateway/src/test/java/api/buyhood/gateway/BuyhoodApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package api.buyhood.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuyhoodApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
