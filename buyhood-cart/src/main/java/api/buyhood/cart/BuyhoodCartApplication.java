6b8b55599030e385a8927ddb7b7b4d869b80022e
sjMun09 <ohoh7391@naver.com>
2025-06-12 17:50:43 +0900

    feat(MFLP-88): validate Cart entity/repository and Redis integration

--- buyhood-cart/src/main/java/api/buyhood/cart/BuyhoodCartApplication.java
+++ buyhood-cart/src/main/java/api/buyhood/cart/BuyhoodCartApplication.java
@@ -2,8 +2,20 @@ package api.buyhood.cart;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(
+	scanBasePackages = {
+		"api.buyhood.cart",
+		"api.buyhood.config",
+		"api.buyhood.filter",
+		"api.buyhood.security"
+	}
+)
+@EnableFeignClients(basePackages = {
+    "api.buyhood.cart.client",  // Cart 모듈 내 Feign
+    "api.buyhood.client"
+})
 public class BuyhoodCartApplication {
 
 	public static void main(String[] args) {
